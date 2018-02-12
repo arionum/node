4963cea396811c69a2475ab26662e92f98bc20b5
ProgrammerDan <programmerdan@gmail.com>
2018-02-12 15:28:18 -0500

    Some fixes and improved display

--- index.php
+++ index.php
@@ -30,16 +30,14 @@ $block=new Block;
 $current=$block->current();
 
 echo "<h3>Arionum Node</h3>";
-echo "System check complete.<br><br> Current block: $current[height]";
+echo "System check complete.<br><br> Current block: $current[height] <br><br> Current difficulty: $current[difficulty]";
 
 echo "<hr />";
 echo "<h4>Worker Status</h4>";
 
 echo "<table>";
 
-echo "<th><td>Worker</td><td>Last Report</td><td>Hashes</td><td>H/s</td><td>Submits</td><td>Finds</td><td>Failures</td><td>Efficiency</td></th>";
-select worker, avg(rate), max(date) from worker_report where date > UNIX_TIMESTAMP() - 300 group by worker having max(│date) > UNIX_TIMESTAMP() - 120;
-select worker, count(*), min(dl), sum(case when confirmed = 1 then 1 else 0 end) as confirmed from worker_discovery group by worker;
+echo "<thead><tr><td>Worker</td><td>Last Report</td><td>Hashes</td><td>H/s</td><td>Submits</td><td>Finds</td><td>Failures</td><td>Efficiency</td></tr></thead><tbody>";
 
 $workers = $db->row("select a.worker as worker, total_hashes, avg_rate, find, submit, failure, latest_date from (select worker, sum(hashes) as total_hashes from worker_report group by worker having max(date) > UNIX_TIMESTAMP() - 120) a "
         ."left join (select worker, avg(rate) as avg_rate, max(date) as latest_date from worker_report where date > UNIX_TIMESTAMP() - 300 group by worker having max(date) > UNIX_TIMESTAMP() - 120) b on a.worker = b.worker "
@@ -55,29 +53,32 @@ if ($workers!==false) {
 	$totals['submit'] += $t['submit'];
 	$totals['find'] += $t['find'];
 	$totals['failure'] += $t['failure'];
-	$totals['eff'] += bcdiv($t['submit'] + $t['find'] + $t['failure'], $t['failure'], 4) * 100;
+	$totals['eff'] += 100 - bcdiv($t['failure'], $t['submit'] + $t['find'] + $t['failure'], 4) * 100;
         $count++;
-    } 
+    }
 
-    echo "<tr><td>Total</td><td>Now</td><td>".$totals['hashes']."</td><td>".$totals['rate']."</td><td>".$totals['submit']."</td><td>".$totals['find']."</td><td>".$totals['failure']."</td><td>".$totals['eff']."</td></tr>";
+    $totals['eff'] = bcdiv($totals['eff'], $count) ;
+
+    echo "<tr><td><b>Total</b></td><td>All Time</td><td>".$totals['hashes']."</td><td>".number_format($totals['rate'],2)."</td><td>".$totals['submit']."</td><td>".$totals['find']."</td><td>".$totals['failure']."</td><td>".number_format($totals['eff'],2)."</td></tr>";
 
     foreach($workers as $t) {
-	$eff = bcdiv($t['submit'] + $t['find'] + $t['failure'], $t['failure'], 4) * 100;
+	$eff = 100.0 - bcdiv($t['failure'], $t['submit'] + $t['find'] + $t['failure'], 4) * 100;
 
-        echo "<tr><td>".$t['worker']."</td><td>".$t['latest_date']."</td><td>".$t['total_hashes']."</td><td>".$t['avg_rate']."</td><td>".$t['submit']."</td><td>".$t['find']."</td><td>".$t['failure']."</td><td>".$eff."</td></tr>";
+        echo "<tr><td>".$t['worker']."</td><td>".date("M/d/Y H:i:s",$t['latest_date'])."</td><td>".$t['total_hashes']."</td><td>".number_format($rate, 2)."</td><td>".$t['submit']."</td><td>".$t['find']."</td><td>".$t['failure']."</td><td>".number_format($eff2,2)."</td></tr>";
     }
 } else {
-    echo "<tr><td colspan=\"8\">No Workers currently reporting.</td></tr>"
+    echo "<tr><td colspan=\"8\">No Workers currently reporting.</td></tr>";
+}
 
-echo "</table>";
+echo "</tbody></table>";
 
 $all_hashes = $db->single("select sum(hashes) as all_hashes from worker_report");
 $all_submit = $db->row("select sum(case when confirmed and dl <= 240 then 1 else 0 end) as all_finds, sum(case when confirmed and dl > 240 then 1 else 0 end) as all_submits, sum(case when not confirmed then 1 else 0 end) as all_failures from worker_discovery");
 
-echo "<ul><li><h4>Data across all miners since tracking began</h4></li>";
-echo "<li><b>Total Hashes: </b><span style=\"font-size: larger;\">$all_hashes</span></li>";
-echo "<li><b>Total Hashes: </b><span style=\"font-size: larger;\">".$all_submit["all_submits"]."</span></li>";
-echo "<li><b>Total Hashes: </b><span style=\"font-size: larger;\">".$all_submit["all_finds"]."</span></li>";
-echo "<li><b>Total Hashes: </b><span style=\"font-size: larger;\">".$all_submit["all_failure"]"</span></li></ul>";
+echo "<h4>Data across all miners since tracking began</h4><ul>";
+echo "<li><b>Hashes: </b><span style=\"font-size: larger;\">$all_hashes</span></li>";
+echo "<li><b>Submits: </b><span style=\"font-size: larger;\">".$all_submit["all_submits"]."</span></li>";
+echo "<li><b>Block Finds: </b><span style=\"font-size: larger;\">".$all_submit["all_finds"]."</span></li>";
+echo "<li><b>Failures: </b><span style=\"font-size: larger;\">".$all_submit["all_failures"]."</span></li></ul>";
 
 ?>
