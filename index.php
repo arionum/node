7293d4b4f0708bc0a8ab2b2ab709f73b402e720d
ProgrammerDan <programmerdan@gmail.com>
2018-02-13 22:02:39 -0500

    Adding submit rate and runtime

--- index.php
+++ index.php
@@ -37,15 +37,15 @@ echo "<h4>Worker Status</h4>";
 
 echo "<table>";
 
-echo "<thead><tr><td>Worker</td><td>Last Report</td><td>Hashes</td><td>H/s</td><td>Submits</td><td>Finds</td><td>Failures</td><td>Efficiency</td></tr></thead><tbody>";
+echo "<thead><tr><td>Worker</td><td>Last Report</td><td>Time Active</td><td>Hashes</td><td>H/s</td><td>Submits</td><td>Finds</td><td>Failures</td><td>Efficiency</td><td>Hash/Attempt</td></tr></thead><tbody>";
 
-$workers = $db->row("select a.worker as worker, total_hashes, avg_rate, find, submit, failure, latest_date from (select worker, sum(hashes) as total_hashes from worker_report group by worker having max(date) > UNIX_TIMESTAMP() - 120) a "
+$workers = $db->row("select a.worker as worker, total_hashes, avg_rate, find, submit, failure, latest_date, life from (select worker, sum(hashes) as total_hashes, (max(date) - min(date)) as life from worker_report group by worker having max(date) > UNIX_TIMESTAMP() - 120) a "
         ."left join (select worker, avg(rate) as avg_rate, max(date) as latest_date from worker_report where date > UNIX_TIMESTAMP() - 300 group by worker having max(date) > UNIX_TIMESTAMP() - 120) b on a.worker = b.worker "
         ."left join (select worker, sum(case when confirmed and dl <= 240 then 1 else 0 end) as find, sum(case when confirmed and dl > 240 then 1 else 0 end) as submit, sum(case when not confirmed then 1 else 0 end) as failure from worker_discovery group by worker) c "
         ."on b.worker = c.worker");
 
 if ($workers!==false) {
-    $totals = array("hashes"=>0,"rate"=>0.0,"submit"=>0,"find"=>0,"failure"=>0,"eff"=>0.0);
+    $totals = array("hashes"=>0,"rate"=>0.0,"submit"=>0,"find"=>0,"failure"=>0,"eff"=>0.0,"drate"=>0.0,"life"=>0);
     $count = 0;
     foreach($workers as $t) {
 	$totals['hashes'] += $t['total_hashes'];
@@ -54,17 +54,30 @@ if ($workers!==false) {
 	$totals['find'] += $t['find'];
 	$totals['failure'] += $t['failure'];
 	$totals['eff'] += 100 - bcdiv($t['failure'], $t['submit'] + $t['find'] + $t['failure'], 4) * 100;
+        $totals['drate'] += bcdiv($t['total_hashes'], $t['submit'] + $t['find'] + $t['failure'], 2);
+        $totals['life'] += $t['life'];
         $count++;
     }
 
-    $totals['eff'] = bcdiv($totals['eff'], $count) ;
+    $totals['eff'] = bcdiv($totals['eff'], $count);
+    $totals['drate'] = bcdiv($totals['drate'], $count);
 
-    echo "<tr><td><b>Total</b></td><td>".date("M d, Y H:i:s")."</td><td>".$totals['hashes']."</td><td>".number_format($totals['rate'],2)."</td><td>".$totals['submit']."</td><td>".$totals['find']."</td><td>".$totals['failure']."</td><td>".number_format($totals['eff'],2)."</td></tr>";
+    $d1 = new DateTime();
+    $d2 = clone $d1;
+    $d2->add(new DateInterval('PT'.$totals['life'].'S');
+    $iv = $d2->diff($d1);
+
+    echo "<tr><td><b>Total</b></td><td>".date("M d, Y H:i:s")."</td><td>".$iv->format("%hH:%IM:%SS")."</td><td>".$totals['hashes']."</td><td>".number_format($totals['rate'],2)."</td><td>".$totals['submit']."</td><td>".$totals['find']."</td><td>".$totals['failure']."</td><td>".number_format($totals['eff'],2)."</td><td>".number_format($totals['drate'],0)."</td></tr>";
 
     foreach($workers as $t) {
 	$eff = 100.0 - bcdiv($t['failure'], $t['submit'] + $t['find'] + $t['failure'], 4) * 100;
+        $drate = bcdiv($t['total_hashes'], $t['submit'] + $t['find'] + $t['failure'], 0);
+        $d3 = new DateTime();
+        $d4 = clone $d3;
+        $d4->add(new DateInterval('PT'.$t['life'].'S');
+        $ix = $d4->diff($d3);
 
-        echo "<tr><td>".$t['worker']."</td><td>".date("H:i:s",$t['latest_date'])."</td><td>".$t['total_hashes']."</td><td>".number_format($t["avg_rate"], 2)."</td><td>".$t['submit']."</td><td>".$t['find']."</td><td>".$t['failure']."</td><td>".number_format($eff,2)."</td></tr>";
+        echo "<tr><td>".$t['worker']."</td><td>".date("H:i:s",$t['latest_date'])."</td><td>".$ix->format("%hH:%IM:%SS)."</td><td>".$t['total_hashes']."</td><td>".number_format($t["avg_rate"], 2)."</td><td>".$t['submit']."</td><td>".$t['find']."</td><td>".$t['failure']."</td><td>".number_format($eff,2)."</td><td>".$drate."</td></tr>";
     }
 } else {
     echo "<tr><td colspan=\"8\">No Workers currently reporting.</td></tr>";
@@ -74,12 +87,14 @@ echo "</tbody></table>";
 
 $all_hashes = $db->single("select sum(hashes) as all_hashes from worker_report");
 $all_submit = $db->row("select sum(case when confirmed and dl <= 240 then 1 else 0 end) as all_finds, sum(case when confirmed and dl > 240 then 1 else 0 end) as all_submits, sum(case when not confirmed then 1 else 0 end) as all_failures from worker_discovery");
+$all_drate = bcdiv($all_hashes, $all_submit["all_submits"] + $all_submit["all_finds"] + $all_submit["all_failures"],0);
 
 echo "<h4>Data across all miners since tracking began</h4><ul>";
 echo "<li><b>Hashes: </b><span style=\"font-size: larger;\">$all_hashes</span></li>";
 echo "<li><b>Submits: </b><span style=\"font-size: larger;\">".$all_submit["all_submits"]."</span></li>";
 echo "<li><b>Block Finds: </b><span style=\"font-size: larger;\">".$all_submit["all_finds"]."</span></li>";
 echo "<li><b>Failures: </b><span style=\"font-size: larger;\">".$all_submit["all_failures"]."</span></li></ul>";
+echo "<li><b>Hash/Attempt: </b><span style=\"font-size: larger;\">".$all_drate."</span></li></ul>";
 
 
 echo "<script>setTimeout(function(){location.reload();}, 15000);</script>";
