7146cfd3c0a7bf623c0d508cf66b4b8dae1ac4c0
pxgamer <owzie123@gmail.com>
2018-09-06 12:00:42 +0100

    Add current blacklisted public keys

--- include/Blacklist.php
+++ include/Blacklist.php
@@ -10,7 +10,12 @@ final class Blacklist
     /**
      * The official list of blacklisted public keys
      */
-    public const PUBLIC_KEYS = [];
+    public const PUBLIC_KEYS = [
+        // phpcs:disable Generic.Files.LineLength
+        'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCvVQcHHCNLfiP9LmzWhhpCHx39Bhc67P5HMQM9cctEFvcsUdgrkGqy18taz9ZMrAGtq7NhBYpQ4ZTHkKYiZDaSUqQ' => 'Faucet Abuser',
+        'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCxYDeQHk7Ke66UB2Un3UMmMoJ7RF5vDZXihdEXi8gk8ZBRAi35aFrER2ZLX1mgND7sLFXKETGTjRYjoHcuRNiJN1g' => 'Octaex Exchange',
+        // phpcs:enable
+    ];
 
     /**
      * Check if a public key is blacklisted
