5906372fc00073380aed98470c5bb2722eb18f79
pxgamer <owzie123@gmail.com>
2018-10-19 09:49:53 +0100

    Add initial peers from the peer file

--- include/config-sample.inc.php
+++ include/config-sample.inc.php
@@ -55,7 +55,35 @@ $_config['transaction_propagation_peers'] = 5;
 $_config['max_test_peers'] = 5;
 
 // The initial peers to sync from in sanity
-$_config['initial_peer_list'] = [];
+$_config['initial_peer_list'] = [
+    'http://peer1.arionum.com',
+    'http://peer2.arionum.com',
+    'http://peer3.arionum.com',
+    'http://peer4.arionum.com',
+    'http://peer5.arionum.com',
+    'http://peer6.arionum.com',
+    'http://peer7.arionum.com',
+    'http://peer8.arionum.com',
+    'http://peer9.arionum.com',
+    'http://peer10.arionum.com',
+    'http://peer11.arionum.com',
+    'http://peer12.arionum.com',
+    'http://peer13.arionum.com',
+    'http://peer14.arionum.com',
+    'http://peer15.arionum.com',
+    'http://peer16.arionum.com',
+    'http://peer17.arionum.com',
+    'http://peer18.arionum.com',
+    'http://peer19.arionum.com',
+    'http://peer20.arionum.com',
+    'http://peer21.arionum.com',
+    'http://peer22.arionum.com',
+    'http://peer23.arionum.com',
+    'http://peer24.arionum.com',
+    'http://peer25.arionum.com',
+    'http://peer26.arionum.com',
+    'http://peer27.arionum.com',
+];
 
 /*
 |--------------------------------------------------------------------------
@@ -72,7 +100,7 @@ $_config['max_mempool_rebroadcast'] = 5000;
 // The number of blocks between rebroadcasting transactions
 $_config['sanity_rebroadcast_height'] = 30;
 
-// Block accepting transfers from addresses blacklisted by the Arionum devs 
+// Block accepting transfers from addresses blacklisted by the Arionum devs
 $_config['use_official_blacklist'] = true;
 
 /*
